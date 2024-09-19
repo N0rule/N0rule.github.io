430ddc15cc05f1c99c61bdb453a011b8bd0906e6
N0rule <N0rule228@gmail.com>
2024-09-19 19:31:55 +0300

    Styling

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-
 document.addEventListener("DOMContentLoaded", function () {
   const themeButton = document.getElementById("theme-button");
   const body = document.body;
-  
+
   // Retrieve theme from cookies
-  const theme = getCookie('theme');
+  const theme = getCookie("theme");
   if (theme) {
     body.className = theme;
   }
@@ -20,17 +19,17 @@ function toggleTheme() {
 
   if (theme === "light") {
     body.className = "dark";
-    setCookie('theme', 'dark', 365);
+    setCookie("theme", "dark", 365);
   } else {
     body.className = "light";
-    setCookie('theme', 'light', 365);
+    setCookie("theme", "light", 365);
   }
 }
 
 // Set a cookie with the given name, value, and expiry date
 function setCookie(name, value, expiryDays) {
   const date = new Date();
-  date.setTime(date.getTime() + (expiryDays * 24 * 60 * 60 * 1000));
+  date.setTime(date.getTime() + expiryDays * 24 * 60 * 60 * 1000);
   document.cookie = `${name}=${value};expires=${date.toUTCString()};path=/`;
 }
 
